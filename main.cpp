104522ed2c7d26664fadc6ee19045312d86225af
Yurok <predbannikov.yurii@gmail.com>
2021-09-02 22:28:56 +0300

    fix smth change

--- main.cpp
+++ main.cpp
@@ -6,46 +6,47 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include "proxyserver.h"
+#include "stdio.h"
 
-#define SA struct sockaddr
 #define	MAXLINE		4096    /* max text line length */
 #define C_PORT 13
 #define ERROR_NUMER_RETURN  -1
+
+#define SERVER_IP4_ADDR "206.168.112.96"
 
 int main(int argc, char **argv)
 {
     int     sockfd, n;
     char    recvline[MAXLINE + 1];
-    struct sockaddr_in servaddr;
-    if(argc != 2) {
-        std::cerr << "Error:" << strerror(errno);
-        return ERROR_NUMER_RETURN;
-    }
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0) {
         std::cerr << "socket error" << std::endl;
         return ERROR_NUMER_RETURN;
     }
-    bzero(&servaddr, sizeof (servaddr));
+
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof (servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(C_PORT);
-    if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        std::cerr << "inet_pton error for" << argv[1];
+    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP4_ADDR);
+    if(inet_pton(AF_INET, SERVER_IP4_ADDR, &servaddr.sin_addr) <= 0) {
+        std::cerr << "inet_pton error for" << SERVER_IP4_ADDR;
         return ERROR_NUMER_RETURN;
     }
 
-    if(connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
-        std::cerr << "connect error";
+    if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        std::cerr << "connect error " << errno;
     }
 
     while ( (n = recv(sockfd, recvline, MAXLINE, 0)) > 0)
-        {
-            recvline[n] = 0;	/* null terminate */
-            if (fputs(recvline, stdout) == EOF)
-                std::cerr << "fputs error";
-        }
-        if (n == SOCKET_ERROR)
-            errexit("recv error\n");
+    {
+        recvline[n] = 0;	/* null terminate */
+        if (fputs(recvline, stdout) == EOF)
+            std::cerr << "fputs error";
+    }
+    if (n < 0)
+        std::cerr << "read error";
 
-        return 0;
+    return 0;
 }
